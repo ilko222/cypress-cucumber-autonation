955127009b0d3a1c8d3453a9449b3857bdbb55c8
Andrii Yavorskyi <andriyvrsk@gmail.com>
2022-05-19 22:04:19 +0300

    Added API tests for filtering functionality

--- cypress/integration/api-testing-jsonplaceholder.js
+++ cypress/integration/api-testing-jsonplaceholder.js
@@ -166,3 +166,65 @@ describe("patch request", () => {
         })
     })
 })
+
+describe("delete request", () => {
+    it("delete random resource", () => {
+        let num = randomize('00');
+        cy.request({
+            method: "DELETE",
+            url: "https://jsonplaceholder.typicode.com/posts/" + num,
+            headers: {
+                accept: "application/json; charset=UTF-8"
+            }
+        }).then(response => {
+            expect(response.status).to.eql(200);
+        })
+    })
+})
+
+describe("filtering resources", () => {
+    it("return all the posts that belong to the one random user", () => {
+        let num = randomize('0');
+        cy.request({
+            method: "GET",
+            url: "https://jsonplaceholder.typicode.com/posts?userId=" + num,
+            headers: {
+                accept: "application/json; charset=UTF-8"
+            }
+        }).then(response => {
+            let body = response.body
+            cy.log(body);
+            for(let i = 0; i<10; i++){
+                cy.log(body[i].id);
+                cy.log(body[i].title);
+                cy.log(body[i].body);
+                cy.log(body[i].userId);
+            };
+            expect(response.status).to.eql(200);
+        })
+    })
+
+    it("return all the photos that belong to the one random user", () => {
+        let num = randomize('0');
+        cy.request({
+            method: "GET",
+            url: "https://jsonplaceholder.typicode.com/albums/1" + num + "/photos",
+            headers: {
+                accept: "application/json; charset=UTF-8"
+            }
+        }).then(response => {
+            let body = response.body
+            cy.log(body)
+            for(let i = 0; i<50; i++){
+                cy.log(body[i].albumId)
+                cy.log(body[i].id)
+                cy.log(body[i].url);
+                cy.log(body[i].thumbnailUrl);
+                cy.log(body[i].title);
+            };
+            expect(response.status).to.eql(200);
+        })
+    })
+})
+
+
